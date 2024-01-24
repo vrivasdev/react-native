48e4ba4d1c56f95a5ba5104c668760cbfd794d40
Victor Rivas <rivas@watsoncreative.com>
2024-01-24 14:31:57 -0400

    Basic components

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hey, user</Text>
+      <LoginForm />
     </View>
   );
 }
